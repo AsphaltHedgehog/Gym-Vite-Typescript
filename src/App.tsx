b0fa1b1ca44bc454d4cbb8553ff5f389a487f3a3
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-05-01 21:29:33 +0300

    started working on OurClasses

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import NavBar from "@/scenes/navbar"
 import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
 import OurClasses from "@/scenes/ourClasses"
+import ContactUs from "./scenes/contactUs";
 
 
 
@@ -37,7 +38,8 @@ function App() {
       />
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
-      <OurClasses setSelectedPage={setSelectedPage}/>
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage}/>
       </div>
   )
 }
